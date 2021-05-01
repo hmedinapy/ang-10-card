6c69c47a31bca7d7de17cfda362be6017b5d45c6
Hugo Medina <hugo.medina@kandasoft.com>
2021-05-01 08:30:53 -0400

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css', "../../node_modules/bootstrap/dist/css/bootstrap.css"]
+  styleUrls: ['./app.component.css', "../../node_modules/bootstrap/dist/css/bootstrap.css", 
+  "./flavor.css"]
 })
 export class AppComponent {
   title = 'angular-card-view';
